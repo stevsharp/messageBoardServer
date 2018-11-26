5fbf33387e5afa43726bb76ef21a3d9aad2e6827
Spiros <Spyros_Ponaris@hotmail.com>
2018-11-26 23:16:03 +0200

    MessageBoardServer

---
+++ messageboardBackend/Controllers/MessagesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using messageboardBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace messageboardBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+
+        public IEnumerable<Message> Get()
+        {
+            return new Message[]{
+                new Message { Owner = "John" , Text ="Hello"} ,
+                new Message { Owner = "Tim" , Text ="Hi"}
+            };
+        }
+    }
+}
---
+++ messageboardBackend/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace messageboardBackend.Models
+{
+    public class Message
+    {
+        public string Owner{ get; set; }
+
+        public string Text { get; set; }
+    }
+}
